564c416eea806f38d9142314e164455cbf6a470b
MinikoCappu <96332603+MinikoCappu@users.noreply.github.com>
2023-12-21 03:39:04 +0800

    Удалил папку так как не работает

--- bot/states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class Gen(StatesGroup):
-    text_prompt = State()
-    cinema_prompt = State()
-    movie_time_prompt = State()
-    Films_prompt = State()
-    afisha_theater_prompt = State()
